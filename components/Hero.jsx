fa65dd61df085a39fe7a3d8803c96f57109885e9
Xavier Perez <xavierperez.06@gmail.com>
2025-07-17 18:24:06 -0300

    New hero image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,16 +10,16 @@ const Hero = ({ title, message }) => {
 
   return (
     <div
-      className="hero-img mb-12 flex h-screen items-center justify-center bg-cover bg-fixed bg-center"
+      className="hero-img mb-12 flex h-screen items-center justify-center bg-cover bg-fixed"
       ref={ref}
     >
       {/* overlay */}
-      <div className="absolute top-0 right-0 bottom-0 left-0 z-2 bg-black/60" />
+      <div className="absolute top-0 right-0 bottom-0 left-0 z-2 bg-black/50" />
       <div className="z-2 -mt-40 p-5 text-white">
         {title && <h2 className="text-5xl font-bold">{title}</h2>}
         <AnimatedText
           text={message}
-          className="inline-block py-5 text-6xl text-white"
+          className="inline-block py-5 text-6xl text-white lg:text-7xl"
           triggerAnimation={isInView}
         />
       </div>
